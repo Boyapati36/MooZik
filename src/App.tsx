4038f85ca644f56a13422b03ace0b65aed4e8f6b
Boyapati36 <boyapatimuralidilipchowdary78@gmail.com>
2024-03-26 13:48:19 -0500

    Changed sideBar to be responsive

--- src/App.tsx
+++ src/App.tsx
@@ -19,17 +19,11 @@ function App() {
 
   return (
     <div className="relative flex">
-      <div className='w-64 flex-none'>
-
-      <Sidebar />
+      <div>
+        <Sidebar />
       </div>
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
-        
-        
-        
-
         <Searchbar />
-
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             <Routes>
@@ -41,9 +35,6 @@ function App() {
               <Route path="/songs/:songid" element={<SongDetails />} />
               <Route path="/search/:searchTerm" element={<Search />} />
             </Routes>
-          </div>
-          <div className="xl:sticky relative top-0 h-fit">
-            <TopPlay />
           </div>
         </div>
       </div>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,52 +1,71 @@
-import React from 'react';
+import React, { useState } from "react";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { MdOutlineDashboard } from "react-icons/md";
+import { RiSettings4Line } from "react-icons/ri";
+import { TbReportAnalytics } from "react-icons/tb";
+import { AiOutlineUser, AiOutlineHeart } from "react-icons/ai";
+import { FiMessageSquare, FiFolder, FiShoppingCart } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
-const Sidebar = () => {
+const Home = () => {
+  const menus = [
+    { name: "dashboard", link: "/", icon: MdOutlineDashboard },
+    { name: "user", link: "/", icon: AiOutlineUser },
+    { name: "messages", link: "/", icon: FiMessageSquare },
+    { name: "analytics", link: "/", icon: TbReportAnalytics, margin: true },
+    { name: "File Manager", link: "/", icon: FiFolder },
+    { name: "Cart", link: "/", icon: FiShoppingCart },
+    { name: "Saved", link: "/", icon: AiOutlineHeart, margin: true },
+    { name: "Setting", link: "/", icon: RiSettings4Line },
+  ];
+  const [open, setOpen] = useState(false);
   return (
-    <aside className="fixed top-0 left-0 z-50 w-64 h-screen bg-gray-900 text-white shadow-lg">
-      
-      
-      <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-4xl lg:text-5xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">MooZik</span></h1>
-      
-
-      
-      <nav className="flex-1">
-        <ul className="py-4 space-y-2">
-          <li>
-            <a
-              href="#"
-              className="flex items-center px-4 py-2 text-gray-300 rounded-lg hover:bg-gray-700"
+    <section className="flex gap-6">
+      <div
+        className={`bg-[#0e0e0e] min-h-screen ${
+          open ? "w-72" : "w-16"
+        } duration-500 text-gray-100 px-4`}
+      >
+        <div className="py-3 flex justify-end">
+          <HiMenuAlt3
+            size={26}
+            className="cursor-pointer"
+            onClick={() => setOpen(!open)}
+          />
+        </div>
+        <div className="mt-4 flex flex-col gap-4 relative">
+          {menus?.map((menu, i) => (
+            <Link
+              to={menu?.link}
+              key={i}
+              className={` ${
+                menu?.margin && "mt-5"
+              } group flex items-center text-sm  gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md`}
             >
-              <span>Account</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex items-center px-4 py-2 text-gray-300 rounded-lg hover:bg-gray-700"
-            >
-              <span>Genres</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex items-center px-4 py-2 text-gray-300 rounded-lg hover:bg-gray-700"
-            >
-              <span>Settings</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex items-center px-4 py-2 text-gray-300 rounded-lg hover:bg-gray-700"
-            >
-              <span>About</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </aside>
+              <div>{React.createElement(menu?.icon, { size: "20" })}</div>
+              <h2
+                style={{
+                  transitionDelay: `${i + 3}00ms`,
+                }}
+                className={`whitespace-pre duration-500 ${
+                  !open && "opacity-0 translate-x-28 overflow-hidden"
+                }`}
+              >
+                {menu?.name}
+              </h2>
+              <h2
+                className={`${
+                  open && "hidden"
+                } absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit  `}
+              >
+                {menu?.name}
+              </h2>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 };
 
-export default Sidebar;
+export default Home;
